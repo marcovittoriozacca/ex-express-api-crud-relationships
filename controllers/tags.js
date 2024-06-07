deff8fb50b02eaabba088fdeadaa7d853c52b0aa
marcovittoriozacca <zaccamarco01@gmail.com>
2024-06-07 16:36:49 +0200

    feat: BONUS CRUD for tag model

--- controllers/tags.js
+++ controllers/tags.js
@@ -16,7 +16,78 @@ const store = async (req, res, next) => {
     }
 }
 
+const index = async (req, res, next) => {
 
+    try{
+        const allTags = await prisma.tag.findMany();
+        res.json({
+            status:200,
+            success:true,
+            tags: allTags
+        });
+    }catch(err){
+        next(err);
+    }
+}
+
+const show = async (req, res, next) => {
+    const id = parseInt(req.params.id);
+
+    try{
+        const tagToShow = await prisma.tag.findUnique({
+            where: {id}
+        })
+        
+        res.json({
+            status:200,
+            success:true,
+            tag_to_show: tagToShow
+        });
+    }catch(err){
+        next(err);
+    }
+}
+
+const update = async (req, res, next) => {
+    const id = parseInt(req.params.id);
+
+    const { name } = req.body;
+
+    try{
+        const tagToUpdate = await prisma.tag.update({
+            where: {id},
+            data: {name}
+        })
+        res.json({
+            status:200,
+            success:true,
+            updated_tag: tagToUpdate
+        });
+    }catch(err){
+        next(err);
+    }
+}
+
+const destroy = async (req, res, next) => {
+    const id = parseInt(req.params.id);
+    try{
+        const tagToDelete = await prisma.tag.delete({
+            where: {id}
+        });
+        res.json({
+            status:200,
+            success:true,
+            deleted_tag: tagToDelete,
+            message: "Deleted successfully"
+        });
+    }catch(err){
+        next(err);
+    }
+}
 module.exports = {
-    store
+    store,
+    index,
+    show,
+    update,
+    destroy
 }
--- routers/tags.js
+++ routers/tags.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
-const { store } = require('../controllers/tags.js');
+const { store, index, show, update, destroy } = require('../controllers/tags.js');
 
 const router = express.Router();
 
+router.get('/', index);
 
 router.post('/', store);
+
+router.get('/:id', show);
+
+router.put('/:id', update);
+
+router.delete('/:id', destroy);
 
 module.exports = router;
