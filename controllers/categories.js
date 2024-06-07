b7789c75c58994831ead524b785acd7d58700463
marcovittoriozacca <zaccamarco01@gmail.com>
2024-06-07 15:13:07 +0200

    feat: BONUS all crud operations for categories

--- controllers/categories.js
+++ controllers/categories.js
@@ -17,7 +17,81 @@ const index = async (req, res, next) => {
 
 }
 
+const store = async (req, res, next) => {
+    const { name } = req.body;
+
+    try{
+        const newCategory = await prisma.category.create({data: {name}})
+        
+        res.json({
+            status:200,
+            success:true,
+            new_category: newCategory
+        })
+    }catch(err){
+        return next(err);
+    }
+    
+
+}
+
+const show = async (req, res, next) => {
+    const id = parseInt(req.params.id);
+
+    try{
+        const categoryToShow = await prisma.category.findUnique({
+            where: {id}
+        })
+        res.json({
+            status:200,
+            success:true,
+            category: categoryToShow
+        })
+    }catch(err){
+        return next(err);
+    }
+}
+
+const update = async (req, res, next) => {
+    const id = parseInt(req.params.id);
+    const { name } = req.body;
+
+    try{
+        const categoryToUpdate = await prisma.category.update({
+            where: {id},
+            data: {name},
+        });
+        return res.json({
+            status:200,
+            success:true,
+            updated_category: categoryToUpdate
+        });
+    }catch(err){
+        return next(err);
+    }
+}
+
+const destroy = async (req, res, next) => {
+    const id = parseInt(req.params.id);
+    try{
+        const categoryToDelete = await prisma.category.delete({
+            where: {id}
+        });
+        return res.json({
+            status:200,
+            success:true,
+            deleted_category: categoryToDelete
+        });
+    }catch(err){
+        next(err);
+    }
+}
+
 
 module.exports = {
     index,
+    store,
+    show,
+    update,
+    destroy
 }
--- routers/categories.js
+++ routers/categories.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const { index } = require('../controllers/categories.js');
+const { index, store, show, update, destroy } = require('../controllers/categories.js');
 const router = express.Router();
 
 router.get('/', index);
+
+router.post('/', store);
+
+router.get('/:id', show);
+
+router.put('/:id', update);
+
+router.delete('/:id', destroy);
+
 
 
 module.exports = router;
